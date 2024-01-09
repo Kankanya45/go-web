8a5a2e980841f874d436c5d7149ee3d5faaaeda8
Kankanya45 <Kankanya45>
2024-01-09 14:06:39 +0700

    update template

--- main.go
+++ main.go
@@ -2,34 +2,39 @@ package main
 
 import (
 	"fmt"
+	"html/template" //template html
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
 func main() {
-	rt := mux.NewRouter()
+	//define template
+	tmpl := template.Must(template.ParseFiles("layout.html"))
 
-	//ex: http://localhost:8080/books/cprogramming/page/1
-	//title = cprogramming
-	// page = 1
-
-	rt.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
-
-		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	// define route
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
 	})
 
-	// ex : http://localhost:80
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	// })
-
-	// fs := http.FileServer(http.Dir("static/"))
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
-
+	//print tem
 	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", rt)
+
+	http.ListenAndServe(":8080", nil)
 }
